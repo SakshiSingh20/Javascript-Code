9639bf67ce31802dee35341983a877fd52f865c8
Sakshi Singh <134781157+SakshiSingh20@users.noreply.github.com>
2023-10-09 05:55:18 +0000

    Learning Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = "144553"
+let accountEmail = "sakshi@google.com"
+var accountPassword = "12365"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 not alllowed
+
+accountEmail = "shark@google.com"
+accountPassword = "23253"
+accountCity = "pune"
+
+// console.log(accountEmail)
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use Var
+because of issue of block scope and functional scope
+*/
