10c3072696979aebd7799eb4b6f0fdf6b89e7eba
Sakshi Singh <134781157+SakshiSingh20@users.noreply.github.com>
2023-10-10 09:51:36 +0000

    Learning String

---
+++ 01_basics/06_string.js
@@ -0,0 +1,43 @@
+// // const name = "sakshi"
+// // const repoCount = 6
+
+
+// // console.log(name + repoCount + " Value");
+
+// // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// const gameName = new String('sak-shark')
+
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// // console.log(gameName.charAt(5));
+// // console.log(gameName.indexOf('h'));
+
+
+// const newString = gameName.substring(0, 3)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+
+// const newStringOne = "   Sakshi    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sakshi'))
+
+// console.log(gameName.split(''));
+
+const icons = '☃★♲';
+
+console.log(icons.codePointAt(1));
